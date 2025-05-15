b7afeb536a1068fcc1b82a2a3ef53fd53f1a6689
itian16 <146409000+itian16@users.noreply.github.com>
2025-05-15 13:01:52 +0800

    updated UI and ANimation

--- script.js
+++ script.js
@@ -12,3 +12,28 @@
      alert('Please fill in all fields.');
    }
  });
+
+ let slideIndex = 0;
+    showSlides();
+  
+    function showSlides() {
+      const slides = document.getElementsByClassName("mySlides");
+      for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+      }
+      slideIndex++;
+      if (slideIndex > slides.length) { slideIndex = 1; }
+      slides[slideIndex - 1].style.display = "block";
+      setTimeout(showSlides, 5000); // Change image every 4 seconds
+    }
+
+    document.addEventListener('DOMContentLoaded', function () {
+    const learnMoreLink = document.getElementById('learnMoreLink');
+    const mofuSection = document.getElementById('mofu');
+
+    learnMoreLink.addEventListener('click', function (e) {
+      e.preventDefault(); // Prevent the default jump
+      mofuSection.classList.add('show');
+      mofuSection.scrollIntoView({ behavior: 'smooth' });
+    });
+  });
